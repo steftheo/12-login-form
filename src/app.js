9ad40a4d8f08ee8bb43f23efded1867c2b3c2b42
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-21 18:34:46 -0500

    Start app and login-form js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/**
+ * This app should do all the stuff needed to start the application
+ * That includes finding particular elements and calling/loading other modules
+ */
+function startApp() {
+
+}
+
+export default startApp;
